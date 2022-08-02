ece170be32160ab06b28044486adb247dca6a920
AbdulMoizJawed <86627657+AbdulMoizJawed@users.noreply.github.com>
2022-08-02 16:05:30 +0500

    Classes And Objects

---
+++ Classes_Objects.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+class Employee
+{
+    
+    private:
+        int a, b, c ;
+    public:
+        int d, e;
+        void setData(int a1, int b1, int c1);//Declaration of a function
+        void getData(){
+            cout<<"the value of a is "<<a<<endl;
+            cout<<"the value of b is "<<b<<endl;
+            cout<<"the value of c is "<<c<<endl;
+            cout<<"the value of d is "<<d<<endl;
+            cout<<"the value of e is "<<e<<endl;
+        }
+};
+
+void Employee :: setData(int a1, int b1, int c1){
+    a=a1;
+    b=b1;
+    c=c1;
+}
+
+
+
+int main(){
+    Employee harry;
+    harry.d =34;
+    harry.e =22;
+    // harry.a = 1; this will give error as a is private 
+    harry.setData(1,2,4);
+    harry.getData();   
+
+ 
+
+
+    return 0;
+}
